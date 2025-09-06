f97746a529393a3b829325f5bc200ecda45beccb
Ansstasia <anastasia@gmail.com>
2025-09-06 12:45:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 10; //создаем переменную  
+            //Console.WriteLine(number); //вывод переменной 
+            //Console.WriteLine("hello world!"); //вывод 
+
+            ////считывать будем снизу с клавы имя 
+            //Console.WriteLine("введите имя: "); //просим ввести имя
+            //string name = Console.ReadLine(); //считываем что человек напишет  
+            //Console.WriteLine($" Ваше имя {name}"); //мы показываем 
+
+            //создать 3 переменные - имя возраст город и вывести
+            //string nam = "denis";
+            //int age = 10;
+            //string city = "volgo";
+            //Console.WriteLine($" Ваше имя {nam}");
+            //Console.WriteLine($" Ваше возраст  {age}");
+            //Console.WriteLine($" Ваше город {city}");
+            //Console.WriteLine("Имя - {0} возраст - {1} город - {2}", nam, age, city);
+
+            //Console.Write("Введите возраст ");
+            //int age = int.Parse(Console.ReadLine());
+            //int age2 = Convert.ToInt32(Console.ReadLine()); 
+            //Console.WriteLine(age);
+
+            //int num = 240;
+            //long num2 = num;
+            //Console.WriteLine(num2);
+
+            //int num = 240;
+            //byte num2 = (byte) num;
+            //Console.WriteLine(num2);
+
+            //int num = 240;
+            //byte num2 = (byte)num;
+            //Console.WriteLine(num2);
+
+            //bool alive = true;
+            //bool isDead = false;
+
+            //double iDouble = 2.4;
+            //float iFloat = 3.3f; //f пишем 
+            //decimal idecimal = 34.34m;
+
+            //object fgf = 'f', "fdd", 34; // обжиг но не 
+
+            //var name = "ew"; //он сам решает какой это тип данных но есть нюанс присвоить значения обязательно 
+            
+            //символы 
+            //char symbol = 'a';
+
+            //Console.WriteLine(int.MinValue); //максимальное значения 
+            
+        }
+    }
+}
